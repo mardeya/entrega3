913ae263be833c531bcc071cf43f432063924d0c
Necrohades <tfuentes2004@gmail.com>
2023-10-23 13:32:21 +0200

    Afegir funció Frank

---
+++ contraste_correlacion.R
@@ -0,0 +1,32 @@
+source("data_tidy.R")
+
+
+contraste_correlacion<- function(df, continente,var1,k){  
+  # Contrastes a realizar, para var1 y var2:
+  # H0: corr_continente=corr_global
+  # H1: corr_continente!=corr_global
+  
+  df<-df %>% select(c("Continent",var1))
+  filter(df,Continent==continente)[,var1] -> continente_df_var1
+  num_paises_cont=table(df$Continent)[continente]
+  n=nrow(df)
+  
+  sample_df=sample_n(df,num_paises_cont,replace=FALSE)
+  
+  nH1_var1=0
+  
+  for (x in 1:k){
+    set.seed(x)
+    
+    sample_df=sample_n(df,num_paises_cont,replace=FALSE)
+    
+    # Test de correlacion
+    # var1
+    pval_var1=cor.test(sample_df[,var1],continente_df_var1)$p.value
+    if (pval_var1<0.05){nH1_var1=nH1_var1+1}
+  }
+  res_test_var1=(nH1_var1<(3/4)*k) # TRUE ~ H0, FALSE ~ H1
+  
+  print(nH1_var1)
+  return (res_test_var1)
+}
